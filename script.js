e788674ec9d023e804e44529d25c2e76d3547d91
Vlad <vle-zur@yandex.ru>
2025-05-15 13:28:28 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -290,7 +290,32 @@ function loadGame(gameId) {
     .then(html => {
       container.innerHTML = html;
       console.log("Загружено:", gameId);
-      if (gameId === 'game-coin') updateBetUI();
+
+      if (gameId === 'game-coin') {
+        updateBetUI();
+
+        document.getElementById('btn-heads')?.addEventListener('click', () => setCoinChoice('heads'));
+        document.getElementById('btn-tails')?.addEventListener('click', () => setCoinChoice('tails'));
+        document.querySelector('.play-btn')?.addEventListener('click', function () { playCoin(this); });
+        document.getElementById('btn-back-coin')?.addEventListener('click', backToMain);
+      }
+
+      if (gameId === 'game-boxes') {
+        const boxes = container.querySelectorAll('.boxes img');
+        boxes.forEach((img, i) => {
+          img.addEventListener('click', () => selectBox(i));
+        });
+        container.querySelector('.btn.back-btn')?.addEventListener('click', backToMain);
+      }
+
+      if (gameId === 'game-dice') {
+        container.querySelector('.play-btn')?.addEventListener('click', rollDice);
+        container.querySelector('.btn.back-btn')?.addEventListener('click', backToMain);
+      }
+
+      if (gameId === 'rules' || gameId === 'partners') {
+        container.querySelector('.btn.back-btn')?.addEventListener('click', backToMain);
+      }
     })
     .catch(err => {
       container.innerHTML = '<p style="color:red;">Ошибка загрузки игры</p>';
