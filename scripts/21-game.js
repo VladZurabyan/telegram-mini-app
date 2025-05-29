4d23887ba551795dcd48a755bf959d5dfa9b85b4
Vlad <vle-zur@yandex.ru>
2025-05-30 02:09:05 +0400

    Update 21-game.js

--- scripts/21-game.js
+++ scripts/21-game.js
@@ -15,7 +15,7 @@
     if (percent > 100) percent = 100;
     text.innerText = `Загрузка: ${percent}%`;
     if (percent === 100) clearInterval(interval);
-  }, 200); // скорость счётчика
+  }, 400); // скорость счётчика
 }
 
 function hideLoader() {
