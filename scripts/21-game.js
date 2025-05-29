5e00cb9e507c6a15db0488dcd4b1dd5b31b358d1
Vlad <vle-zur@yandex.ru>
2025-05-30 01:47:16 +0400

    Update 21-game.js

--- scripts/21-game.js
+++ scripts/21-game.js
@@ -14,6 +14,7 @@
         const container = document.getElementById("blackjack-canvas-container");
         if (!container) return;
 showLoader(); // ✅ Показать красивый лоадер
+        await new Promise(requestAnimationFrame);
         container.innerHTML = "";
         container.style.display = "block";
 
@@ -30,6 +31,7 @@ showLoader(); // ✅ Показать красивый лоадер
         container.appendChild(app.view);
         await loadCardAssets();
         setupScene();
+        await new Promise(requestAnimationFrame);
          hideLoader(); // ✅ Скрыть лоадер только когда всё готово
     }
 
